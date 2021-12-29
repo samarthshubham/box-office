1413485e00ddb04c069cda58ec71282a0384f4b3
samarthshubham <shubhamsamarth@outlook.com>
2021-12-29 22:20:54 +0530

    added loading and error message to the show page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { Switch, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Starred from './pages/Starred';
-import Show from './pages/Show';
 
 function App() {
   return (
@@ -14,10 +13,6 @@ function App() {
 
       <Route exact path="/starred">
         <Starred />
-      </Route>
-
-      <Route exact path="/show/:id">
-        <Show />
       </Route>
 
       <Route>
