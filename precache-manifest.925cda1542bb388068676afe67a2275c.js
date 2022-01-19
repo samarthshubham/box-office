fca9f141a76c9b32908e7a7e9f1e022066195f2b
samarthshubham <shubhamsamarth@outlook.com>
2022-01-19 19:47:30 +0530

    Updates

---
+++ precache-manifest.925cda1542bb388068676afe67a2275c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7a276d60af069e12ec5b55fec034e71",
+    "url": "/box-office/index.html"
+  },
+  {
+    "revision": "12dd41ed35b8ac163ea5",
+    "url": "/box-office/static/css/main.93052401.chunk.css"
+  },
+  {
+    "revision": "610bdc9597ae3714515e",
+    "url": "/box-office/static/js/2.f0dcf50e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/box-office/static/js/2.f0dcf50e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12dd41ed35b8ac163ea5",
+    "url": "/box-office/static/js/main.e1bac01a.chunk.js"
+  },
+  {
+    "revision": "c850b6c555a3d841251e",
+    "url": "/box-office/static/js/runtime-main.e4c5afaf.js"
+  }
+]);
